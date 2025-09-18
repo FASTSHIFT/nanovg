468795794ff586594226b815d69cbb1edfd0a992
pengyiqiang <pengyiqiang@xiaomi.com>
2025-09-18 14:37:12 +0800

    feat: add egl init error check

--- example/example_rpi.c
+++ example/example_rpi.c
@@ -1,15 +1,15 @@
-#include <stdio.h>
-#include <bcm_host.h>
-#include <GLES/gl.h>
-#include <GLES2/gl2.h>
+#include "nanovg.h"
 #include <EGL/egl.h>
 #include <EGL/eglext.h>
-#include "nanovg.h"
+#include <GLES/gl.h>
+#include <GLES2/gl2.h>
+#include <bcm_host.h>
+#include <stdio.h>
 
 #define NANOVG_GLES2_IMPLEMENTATION
+#include "demo.h"
 #include "nanovg_gl.h"
 #include "nanovg_gl_utils.h"
-#include "demo.h"
 #include "perf.h"
 
 #include <time.h>
@@ -21,14 +21,23 @@ static EGLContext context;
 static int blowup = 0;
 static int premult = 0;
 
-static void init_egl(int width, int height) {
+static int init_egl(int width, int height)
+{
     static EGL_DISPMANX_WINDOW_T nativewindow;
-    
+
     bcm_host_init();
-    
+
     display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
-    eglInitialize(display, NULL, NULL);
-    
+    if (display == EGL_NO_DISPLAY) {
+        printf("Failed to get EGL display: %d\n", eglGetError());
+        return -1;
+    }
+
+    if (!eglInitialize(display, NULL, NULL)) {
+        printf("Failed to initialize EGL: %d\n", eglGetError());
+        return -1;
+    }
+
     EGLint attribs[] = {
         EGL_RED_SIZE, 8,
         EGL_GREEN_SIZE, 8,
@@ -38,22 +47,29 @@ static void init_egl(int width, int height) {
         EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT,
         EGL_NONE
     };
-    
+
     EGLConfig config;
     EGLint num_configs;
-    eglChooseConfig(display, attribs, &config, 1, &num_configs);
-    
+    if (!eglChooseConfig(display, attribs, &config, 1, &num_configs)) {
+        printf("Failed to choose EGL config: %d\n", eglGetError());
+        return -1;
+    }
+
     EGLint context_attribs[] = {
         EGL_CONTEXT_CLIENT_VERSION, 2,
         EGL_NONE
     };
-    
+
     context = eglCreateContext(display, config, EGL_NO_CONTEXT, context_attribs);
-    
+    if (context == EGL_NO_CONTEXT) {
+        printf("Failed to create EGL context: %d\n", eglGetError());
+        return -1;
+    }
+
     // Create dispmanx window
-    VC_RECT_T dst_rect = {0, 0, width, height};
-    VC_RECT_T src_rect = {0, 0, width << 16, height << 16};
-    
+    VC_RECT_T dst_rect = { 0, 0, width, height };
+    VC_RECT_T src_rect = { 0, 0, width << 16, height << 16 };
+
     DISPMANX_DISPLAY_HANDLE_T dispman_display = vc_dispmanx_display_open(0);
     DISPMANX_UPDATE_HANDLE_T dispman_update = vc_dispmanx_update_start(0);
     DISPMANX_ELEMENT_HANDLE_T dispman_element = vc_dispmanx_element_add(
@@ -61,30 +77,44 @@ static void init_egl(int width, int height) {
         0, &dst_rect, 0,
         &src_rect, DISPMANX_PROTECTION_NONE,
         0, 0, DISPMANX_NO_ROTATE);
-    
+
     nativewindow.element = dispman_element;
     nativewindow.width = width;
     nativewindow.height = height;
     vc_dispmanx_update_submit_sync(dispman_update);
-    
+
     surface = eglCreateWindowSurface(display, config, &nativewindow, NULL);
-    eglMakeCurrent(display, surface, surface, context);
+    if (surface == EGL_NO_SURFACE) {
+        printf("Failed to create EGL surface: %d\n", eglGetError());
+        return -1;
+    }
+
+    if (!eglMakeCurrent(display, surface, surface, context)) {
+        printf("Failed to make EGL context current: %d\n", eglGetError());
+        return -1;
+    }
+
+    return 0;
 }
 
-static float getTime(void) {
+static float getTime(void)
+{
     struct timespec ts;
     clock_gettime(CLOCK_MONOTONIC, &ts);
     return (float)ts.tv_sec + (float)ts.tv_nsec / 1000000000.0f;
 }
 
-int main() {
+int main()
+{
     DemoData data;
     NVGcontext* vg = NULL;
     PerfGraph fps;
     double prevt = 0;
     int width = 480, height = 480;
 
-    init_egl(width, height);
+    if (init_egl(width, height) == -1) {
+        return -1;
+    }
     initGraph(&fps, GRAPH_RENDER_FPS, "Frame Time");
 
     vg = nvgCreateGLES2(NVG_ANTIALIAS | NVG_STENCIL_STROKES | NVG_DEBUG);
@@ -99,7 +129,7 @@ int main() {
     prevt = getTime();
 
     while (1) {
-        double mx = width/2, my = height/2, t, dt;
+        double mx = width / 2, my = height / 2, t, dt;
         float pxRatio = 1.0f;
 
         t = getTime();
@@ -110,10 +140,10 @@ int main() {
         // Update and render
         glViewport(0, 0, width, height);
         if (premult)
-            glClearColor(0,0,0,0);
+            glClearColor(0, 0, 0, 0);
         else
             glClearColor(0.3f, 0.3f, 0.32f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
         glEnable(GL_BLEND);
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
@@ -122,7 +152,7 @@ int main() {
 
         nvgBeginFrame(vg, width, height, pxRatio);
         renderDemo(vg, mx, my, width, height, t, blowup, &data);
-        renderGraph(vg, 5,5, &fps);
+        renderGraph(vg, 5, 5, &fps);
         nvgEndFrame(vg);
 
         eglSwapBuffers(display, surface);
